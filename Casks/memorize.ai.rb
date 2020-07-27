530f870fef2b4e429ad8ff7ec92b690957a8538c
kenmueller <kenmueller0@gmail.com>
2020-07-26 20:56:14 -0700

    converted to tabs

--- Casks/memorize.ai.rb
+++ Casks/memorize.ai.rb
@@ -1,11 +1,11 @@
 cask 'memorize.ai' do
-  version '1.0.0'
-  sha256 '2ab0ab7891ce5a012d503e23c585a53453eadcbd5384b654520fd6a242200169'
-
-  url "https://github.com/memorize-ai/desktop/releases/download/v#{version}/memorize.ai-#{version}.dmg"
-  name 'memorize.ai'
-  homepage 'https://memorize.ai/'
-  appcast 'https://github.com/memorize-ai/desktop/releases.atom'
-
-  app 'memorize.ai.app'
+	version '1.0.0'
+	sha256 '2ab0ab7891ce5a012d503e23c585a53453eadcbd5384b654520fd6a242200169'
+	
+	url "https://github.com/memorize-ai/desktop/releases/download/v#{version}/memorize.ai-#{version}.dmg"
+	name 'memorize.ai'
+	homepage 'https://memorize.ai'
+	appcast 'https://github.com/memorize-ai/desktop/releases.atom'
+	
+	app 'memorize.ai.app'
 end
